4bd5abf665ad246b6be42b42c35548210a40bafd
bandaloo <bandaloo@icloud.com>
2019-08-02 09:25:23 +0900

    added GPS location

---
+++ raycast AR play sound/Assets/Script/GPS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+    public static GPS instance { set; get; }
+    public float latitude;
+    public float longitude;
+    public float timesStarted = 0;
+
+    private void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    // why does this need to return an IEnumerator?
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("user has not enabled location services");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("unable to determine device location");
+            yield break;
+        }
+
+        UpdatePosition();
+
+        timesStarted++;
+
+        yield break;
+    }
+
+    public void UpdatePosition()
+    {
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+    }
+}
---
+++ raycast AR play sound/Assets/Script/UpdateGPSText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour
+{
+    public Text coordinates;
+    public Text timesUpdatedText;
+    public Text timesStartedText;
+    public int timesUpdated = 0;
+
+    private int stepsSinceUpdate = 0;
+
+    private const int maxStep = 60;
+
+    private void Update()
+    {
+        timesUpdated++; // TODO get rid of this
+        stepsSinceUpdate++;
+        if (stepsSinceUpdate >= maxStep)
+        {
+            stepsSinceUpdate = 0;
+            GPS.instance.UpdatePosition();
+        }
+        coordinates.text = "Lat: " + GPS.instance.latitude.ToString() + " Lon: " + GPS.instance.longitude.ToString();
+        timesUpdatedText.text = "times updated: " + timesUpdated.ToString();
+        timesStartedText.text = "times started: " + GPS.instance.timesStarted.ToString();
+    }
+}
