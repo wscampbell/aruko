8d0e260753b026fb4c3f5f725bd5d084194c7400
bandaloo <bandaloo@icloud.com>
2019-08-02 14:23:13 +0900

    added UI stuff and test stuff

---
+++ raycast AR play sound/Assets/Editor/TestTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class TestTest
+{
+    [Test]
+    public void TestTest_Test()
+    {
+
+    }
+}
--- raycast AR play sound/Assets/Script/UpdateGPSText.cs
+++ raycast AR play sound/Assets/Script/UpdateGPSText.cs
@@ -1,30 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UpdateGPSText : MonoBehaviour
 {
     public Text coordinates;
     public Text timesUpdatedText;
-    public Text timesStartedText;
     public int timesUpdated = 0;
-
     private int stepsSinceUpdate = 0;
-
     private const int maxStep = 60;
 
     private void Update()
     {
-        timesUpdated++; // TODO get rid of this
         stepsSinceUpdate++;
         if (stepsSinceUpdate >= maxStep)
         {
             stepsSinceUpdate = 0;
+            timesUpdated++; // TODO get rid of this
             GPS.instance.UpdatePosition();
         }
-        coordinates.text = "Lat: " + GPS.instance.latitude.ToString() + " Lon: " + GPS.instance.longitude.ToString();
-        timesUpdatedText.text = "times updated: " + timesUpdated.ToString();
-        timesStartedText.text = "times started: " + GPS.instance.timesStarted.ToString();
+        coordinates.text = "lat: " + GPS.instance.latitude.ToString() + " lon: " + GPS.instance.longitude.ToString();
+        timesUpdatedText.text = "Times updated: " + timesUpdated.ToString();
     }
 }
