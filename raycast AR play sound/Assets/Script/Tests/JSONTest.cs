e20be1fee7a285edde137b5d2f721ba28105470b
bandaloo <bandaloo@icloud.com>
2019-08-20 12:49:06 +0900

    added full json string with new format

--- raycast AR play sound/Assets/Script/Tests/JSONTest.cs
+++ raycast AR play sound/Assets/Script/Tests/JSONTest.cs
@@ -18,7 +18,7 @@ namespace Tests
         */
         static string deadSimpleRegionJSON = "{\"name\":\"testname\",\"points\":[[123.4, 567.8]]}";
 
-        static string editourRegionJSON = @"{  
+        static string editourBeautifulRegionJSON = @"{  
             ""name"":""Beautiful Region"",
             ""points"":[  
                 {  
@@ -44,7 +44,32 @@ namespace Tests
             ]
         }";
 
-        static string editourFullJSON = "abc123:" + editourRegionJSON;
+        static string editourGorgeousRegionJSON = @"{  
+            ""name"":""Gorgeous Region"",
+            ""points"":[  
+                {  
+                    ""lat"":36.04025698454798,
+                    ""lng"":136.73188900947574
+                },
+                {  
+                    ""lat"":36.03867580927915,
+                    ""lng"":136.7315242290497
+                },
+                {  
+                    ""lat"":36.039150165072655,
+                    ""lng"":136.733197927475
+                }
+            ],
+            ""audio"":[  
+                ""gorgeous-tour.mp3""
+            ],
+            ""images"":[  
+                ""valley.jpg"",
+                ""gorge.jpeg""
+            ]
+        }";
+
+        static string editourFullJSON = "{\"regions\":[" + editourBeautifulRegionJSON + "," + editourGorgeousRegionJSON + "]}";
 
         [Test]
         public void JSONTestSimplePasses()
@@ -54,8 +79,8 @@ namespace Tests
         [Test]
         public void deserializeEditourRegionTest()
         {
-            Debug.Log(editourRegionJSON);
-            EditourRegion editourRegion = JSONHelper.JSONToEditourRegion(editourRegionJSON);
+            Debug.Log(editourBeautifulRegionJSON);
+            EditourRegion editourRegion = JSONHelper.JSONToEditourRegion(editourBeautifulRegionJSON);
             Assert.AreEqual("Beautiful Region", editourRegion.name);
             Assert.AreEqual(3, editourRegion.points.Count);
             Assert.AreEqual(1, editourRegion.audio.Count);
@@ -77,6 +102,12 @@ namespace Tests
             TestSerializable testSerializable = JSONHelper.testSerializable("{\"name\":\"test\",\"num\":1234.5}");
             Assert.AreEqual("test", testSerializable.name);
             Assert.AreEqual(1234.5, testSerializable.num);
+        }
+
+        [Test]
+        public void literallyJustPrintOutFull()
+        {
+            Debug.Log(editourFullJSON);
         }
     }
 }
