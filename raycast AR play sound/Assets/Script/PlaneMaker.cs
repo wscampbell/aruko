c9c9c218ea136126fe0695db6d57d276fe6880d1
bandaloo <bandaloo@icloud.com>
2019-08-23 10:16:09 +0900

    added plane maker for generating game objects from images

---
+++ raycast AR play sound/Assets/Script/PlaneMaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public static GameObject makePicPlane(string filename)
+    {
+        string pathName = "Tour/" + filename;
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        // TODO change this material to something not null
+        plane.GetComponent<Renderer>().material.mainTexture = pathName;
+
+        return plane;
+    }
+}
