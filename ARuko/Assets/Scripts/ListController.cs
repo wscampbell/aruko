afd220cef588c8b2b59202cd35ad3551e1bc9050
Joseph Petitti <jppetitti@wpi.edu>
2019-09-17 15:28:08 +0900

    started chapters UI

---
+++ ARuko/Assets/Scripts/ListController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListController : MonoBehaviour
+{
+    public Sprite[] chapterImages;
+    public GameObject contentPanel;
+    public GameObject listItemPrefab;
+
+    ArrayList chapters;
+
+    void start()
+    {
+        chapters = new ArrayList() {
+            new ListItem(chapterImages[0], "1", "INTRO"),
+            new ListItem(chapterImages[1], "2", "ASHIKAGA"),
+            new ListItem(chapterImages[2], "3", "ZEN BUDDHISM")
+        };
+
+        foreach(ListItem li in chapters) {
+            GameObject newChapter = Instantiate(listItemPrefab) as GameObject;
+            ListItemController controller = newChapter.GetComponent<ListItemController>();
+            controller.icon.sprite = li.icon;
+            controller.itemName.text = li.itemName;
+            controller.number.text = li.number;
+            newChapter.transform.parent = contentPanel.transform;
+            newChapter.transform.localScale = Vector3.one;
+
+        }
+    }
+}
---
+++ ARuko/Assets/Scripts/ListItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ListItem
+{
+    public Sprite icon;
+    public string number;
+    public string itemName;
+
+    public ListItem(Sprite icon, string number, string itemName)
+    {
+        this.icon = icon;
+        this.number = number;
+        this.itemName = itemName;
+    }
+}
