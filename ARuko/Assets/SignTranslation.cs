6ddaa418548c4a464f56bc034211a7a7e64e4846
Will Campbell <wscampbell@wpi.edu>
2019-09-23 15:28:54 +0900

    ar translation button works

---
+++ ARuko/Assets/SignTranslation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignTranslation : MonoBehaviour
+{
+    [SerializeField] GameObject TranslationZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TranslationZone.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.tag);
+                if (hit.collider.tag == "Info")
+                {
+                    hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+                }
+            }
+        }
+    }
+}
