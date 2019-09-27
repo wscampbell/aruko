f79c46d07463cabb37f163ffb1bb12602d1bdc8a
bandaloo <bandaloo@icloud.com>
2019-09-27 15:48:56 +0900

    added rotation to model

---
+++ ARuko/Assets/SpinKinkakuji.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpinKinkakuji : MonoBehaviour
+{
+
+    private float rotateValue = 1;
+    void Update()
+    {
+        this.gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, rotateValue);
+        rotateValue += 0.13333f;
+    }
+}
